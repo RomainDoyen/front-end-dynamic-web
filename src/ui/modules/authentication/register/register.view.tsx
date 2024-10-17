641cd3b7f5d005e6c1bb943ced4b22c607f89342
RomainDoyen <romain.doyen97@gmail.com>
2024-10-17 13:56:23 +0400

    feat: Creation of the Register page design

--- src/ui/modules/authentication/register/register.view.tsx
+++ src/ui/modules/authentication/register/register.view.tsx
@@ -1,8 +1,22 @@
+import Container from "@/ui/components/container/container"
+import Image from "next/image"
 
 export default function registerview() {
   return (
-    <div>
-      Register View
-    </div>
+    <Container className="grid grid-cols-2 gap-20 mb-32">
+      <div>
+        <div className="realtive w-full h-[531px]">
+          <Image 
+            fill
+            src="/svg/character-1.svg" 
+            alt="logo"
+            className=""
+          />
+        </div>
+      </div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, veniam. Saepe illo consectetur non commodi!
+      </div>
+    </Container>
   )
 }
