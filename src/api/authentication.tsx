2b7c11792936f37a24971227308cb067cd0936fa
RomainDoyen <romain.doyen97@gmail.com>
2024-10-18 17:27:09 +0400

    feat: Password recovery procedure with Firebase

--- src/api/authentication.tsx
+++ src/api/authentication.tsx
@@ -1,6 +1,11 @@
 import { auth } from "@/config/firebase.config";
 import { FirebaseError } from "firebase/app";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut,
+  sendPasswordResetEmail,
+} from "firebase/auth";
 
 export const firebaseCreateUser = async (email: string, password: string) => {
   try {
@@ -31,6 +36,19 @@ export const firebaseLogin = async (email: string, password: string) => {
 export const firebaseLogout = async () => {
   try {
     await signOut(auth);
+    return { data: true };
+  } catch (error) {
+    const firebaseError = error as FirebaseError;
+    return { error: {
+      code: firebaseError.code,
+      message: firebaseError.message
+    }};
+  }
+}
+
+export const firebaseForgetPassword = async (email: string) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
     return { data: true };
   } catch (error) {
     const firebaseError = error as FirebaseError;
--- src/ui/modules/authentication/forget-password/forget-password.container.tsx
+++ src/ui/modules/authentication/forget-password/forget-password.container.tsx
@@ -2,21 +2,39 @@ import Forgetpasswordview from "./forget-password.view"
 import { SubmitHandler, useForm } from "react-hook-form";
 import { ForgetPasswordFormFieldType } from "@/types/forms";
 import Usetoggle from "@/hooks/use-toggle";
+import { toast } from "react-toastify";
+import { firebaseForgetPassword } from "@/api/authentication";
+import { useRouter } from "next/router";
 
 export default function Forgetpasswordcontainer() {
 
   const { value: isLoading, setValue: setIsLoading } = Usetoggle({});
 
+  const router = useRouter();
+
   const {  
     handleSubmit, 
     formState: { errors },
     register,
-    setError,
     reset,
   } = useForm<ForgetPasswordFormFieldType>()
+
+  const handleResetPassword = async ({email}: ForgetPasswordFormFieldType) => {
+    const { error } = await firebaseForgetPassword(email);
+    if (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+      return;
+    }
+    toast.success(`Un email de réinitialisation de mot de passe a été envoyé à ${email}`);
+    setIsLoading(false);
+    reset();
+    router.push("/connexion");
+  }
   
   const onSubmit: SubmitHandler<ForgetPasswordFormFieldType> = async (formData) => {
     setIsLoading(true);
+    handleResetPassword(formData);
   }
 
   return (
